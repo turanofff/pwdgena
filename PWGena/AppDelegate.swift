d629c7f0b2271faa8e3fec7a7bc20e59274e20ec
aturanov <art.turanov@gmail.com>
2020-05-06 11:48:20 +1000

    Initial Commit

---
+++ PWGena/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PWGena
+//
+//  Created by Artem Turanov on 6/5/20.
+//  Copyright © 2020 Artem Turanov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PWGena/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PWGena
+//
+//  Created by Artem Turanov on 6/5/20.
+//  Copyright © 2020 Artem Turanov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
